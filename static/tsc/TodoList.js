653982475a5d7207dd84725726ee6a712f5c3675
iMasanari <14dd023@gmail.com>
2017-02-02 20:43:18 +0900

    answer.mdの追加など

--- static/tsc/TodoList.js
+++
@@ -1,7 +0,0 @@
-export default TodoList;
-import * as React from 'react';
-class TodoList extends React.Component {
-    render() {
-        return React.createElement("div", null, this.props.data);
-    }
-}
